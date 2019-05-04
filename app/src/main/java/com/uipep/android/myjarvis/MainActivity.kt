7123e4e0bddb9e0a485fa469c8ce8e0975607edf
Burhan ud din Drabu <burhan@uipep.com>
2019-05-05 00:23:51 +0530

    pushed without yml

--- app/src/main/java/com/uipep/android/myjarvis/MainActivity.kt
+++ app/src/main/java/com/uipep/android/myjarvis/MainActivity.kt
@@ -2,11 +2,14 @@ package com.uipep.android.myjarvis
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(javaClass.simpleName, "onCreate: implemented ")
     }
 }
